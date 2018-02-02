924972ef285a9014af852ab1f93a5d42135bc7d4
lourenco-tx <lourenco.tx@gmail.com>
2018-02-02 01:46:58 +0000

    admin vs categorias

--- public_html/admin-categories.php
+++ public_html/admin-categories.php
@@ -1,9 +1,14 @@
 <?php
+namespace Hcode\Model;
+// Exibindo todos os erros e warnings para facilitar a identificação de erros
+ini_set('display_errors', true);
+error_reporting(E_ALL);
 
     use \Hcode\PageAdmin;
     use \Hcode\Model\User;
     use \Hcode\Model\Category;
-
+    use \Hcode\Model\Product;
+    
     $app->get("/admin/categories", function()
     {
         User::verifyLogin();
@@ -63,15 +68,39 @@
         exit;
     });
     
-    $app->get("/categories/:idcategory", function($idcategory)
-    {
+    $app->get("/admin/categories/:idcategory/products", function($idcategory){
+        User::verifyLogin();
         $category = new Category;
         $category->get((int)$idcategory);
-        $page = new Page();
-	    $page->setTpl("category", [
+        $page = new PageAdmin();
+	    $page->setTpl("categories-products", [
 	        'category'=>$category->getvalues(),
-	        'products'=>[]
+	        'productsRelated'=>$category->getProducts(),
+	        'productsNotRelated'=>$category->getProducts(false)
 	    ]);
     });
-
+    
+    $app->get("/admin/categories/:idcategory/products/:idproduct/add", function($idcategory, $idproduct)
+    {
+        User::verifyLogin();
+        $category = new Category;
+        $category->get((int)$idcategory);
+        $product = new Product();
+        $product->get((int)$idproduct);
+        $category->addProduct($product);
+        header("Location: /admin/categories/".$idcategory."/products");
+        exit;
+    });
+    
+    $app->get("/admin/categories/:idcategory/products/:idproduct/remove", function($idcategory, $idproduct)
+    {
+        User::verifyLogin();
+        $category = new Category;
+        $category->get((int)$idcategory);
+        $product = new Product();
+        $product->get((int)$idproduct);
+        $category->removeProduct($product);
+        header("Location: /admin/categories/".$idcategory."/products");
+        exit;
+    });
 ?>
--- public_html/vendor/hcodebr/php-classes/src/Model/Product.php
+++ public_html/vendor/hcodebr/php-classes/src/Model/Product.php
@@ -16,6 +16,18 @@ class Product extends Model {
         return $sql->select("SELECT * FROM tb_products ORDER BY desproduct");
     }
     
+    public static function checkList($list)
+    {
+        foreach ($list as &$row)
+        {
+            $p = new Product();
+            $p->setData($row);
+            $row = $p->getValues();
+        }
+        
+         return $list;
+    }
+    
     public function save()
     {
         $sql = new Sql();
@@ -25,7 +37,7 @@ class Product extends Model {
             ":vlprice"=>$this->getvlprice(),
             ":vlwidth"=>$this->getvlwidth(),
             ":vlheight"=>$this->getvlheight(),
-            ":vllenght"=>$this->getvllength(),
+            ":vllength"=>$this->getvllength(),
              ":vlweight"=>$this->getvlweight(),
             ":desurl"=>$this->getdesurl()
             ));
@@ -48,8 +60,62 @@ class Product extends Model {
         $sql->query("DELETE FROM tb_products WHERE idproduct = :idproduct", [
            ':idproduct'=>$this->getidproduct() 
         ]);
-     
     }
+    
+    public function checkPhoto()
+    {
+        if (file_exists(
+            $_SERVER['DOCUMENT_ROOT'] . DIRECTORY_SEPARATOR .
+            "res" . DIRECTORY_SEPARATOR .
+            "site" . DIRECTORY_SEPARATOR .
+            "img" . DIRECTORY_SEPARATOR .
+            "products" . DIRECTORY_SEPARATOR .
+            $this->getidproduct() . ".jpg"
+            ))
+            {
+                $url = "/res/site/img/products/" . $this->getidproduct() . ".jpg";
+            } else {
+                $url = "/res/site/img/product.jpg";
+            }
+            
+        return $this->setdesphoto($url);
+            
+    }
+    
+    public function getValues()
+    {
+        $this->checkPhoto();
+        $values = parent::getValues();
+        return $values;
+    }
+    
+   public function setPhoto($file) {
+    $extension = explode('.', $file['name']);
+    $extension = end($extension);
+    
+    switch ($extension) {
+ 
+        case "jpg":
+        case "jpeg":
+        $image = imagecreatefromjpeg($file["tmp_name"]);
+        break;
+        case "gif":
+        $image = imagecreatefromgif($file["tmp_name"]);
+        break;
+        case "png":
+        $image = imagecreatefrompng($file["tmp_name"]);
+        break;
+    }
+    $dist = $_SERVER['DOCUMENT_ROOT'] . DIRECTORY_SEPARATOR . 
+        "res" .  DIRECTORY_SEPARATOR .
+        "site" . DIRECTORY_SEPARATOR . 
+        "img" . DIRECTORY_SEPARATOR . 
+        "products" . DIRECTORY_SEPARATOR .
+    $this->getidproduct() . ".jpg";
+    imagejpeg($image, $dist);
+    imagedestroy($image);
+    $this->checkPhoto();
+  }
     
 }
 
